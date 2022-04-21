679d6ac94d40abb3514ba11cc6f1a9b52d03122d
chamomiletay <chamomile.tay@gmail.com>
2022-04-20 22:17:56 -0400

    moved index.js to root folder (app wouldn't read .env url), created json data

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 const express = require('express');
+const methodOverride = require('method-override');
+
 const app = express();
-const journalController = require('./controllers/journalController')
+const journalController = require('./backend/controllers/journalController')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 //*note: CHANGE CONTROLLER NAME LATER
 app.use('/journal/', journalController);
 
