69ea3f451011b4d680ce8991af044f7f344e3769
chamomiletay <chamomile.tay@gmail.com>
2022-04-27 17:56:46 -0400

    Moved index.js to backend folder

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const methodOverride = require('method-override');
 
 const app = express();
-const journalController = require('./backend/controllers/journalController')
+const journalController = require('./controllers/journalController')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
